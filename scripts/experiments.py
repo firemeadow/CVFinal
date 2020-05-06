dec6aeec3d50141404a2c68b16cb3bec806dd181
lmi12 <lmi12@case.edu>
2020-05-05 21:18:27 -0400

    algorithm finalized, prints outputs to csv

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -1,16 +1,21 @@
 import numpy as np
 from imageio import imread
+import pandas as pd
 import matplotlib.pyplot as plt
 import algs
 
 
-def run_alg(pic_name, start_pic, end_pic, alg_name, n_points, patch_size, windowed=False):
-    im0 = imread("../data/" + pic_name + "/view" + start_pic + ".png")
-    im1 = imread("../data/" + pic_name + "/view" + end_pic + ".png")
+def run_alg(pic_name, start_pic, end_pic, alg_name, n_points, patch_size, noise=None, windowed=False):
+    im0 = imread("../data/" + pic_name + "/view" + str(start_pic) + ".png")
+    im1 = imread("../data/" + pic_name + "/view" + str(end_pic) + ".png")
     u_arr = []
     v_arr = []
     random_x = np.random.choice(np.arange(im0.shape[0]), n_points)
     random_y = np.random.choice(np.arange(im0.shape[1]), n_points)
+
+    if noise is not None:
+        im0 = np.add(im0, np.round(np.random.normal(0, noise, np.shape(im0)), 0).astype(int))
+        im1 = np.add(im1, np.round(np.random.normal(0, noise, np.shape(im1)), 0).astype(int))
 
     for x, y in zip(random_x, random_y):
         # print("Point: " + str(x) + ", " + str(y))
@@ -27,14 +32,33 @@ def run_alg(pic_name, start_pic, end_pic, alg_name, n_points, patch_size, window
     return ui, vi
 
 
+# ['ssd', 'srd', 'sad', 'bgscsd', 'ncc']
 if __name__ == "__main__":
-    picture = 'baby1'
+    pictures = ['baby1', 'baby2', 'aloe', 'plastic']
     start = 0
     end = 1
-    algorithm_name = 'ncc'
-    num_points = 500
-    patch_size = 21
-    algs = ['ssd', 'srd', 'sad', 'bgscsd', 'ncc']
-    for i in range(1):
-        for alg in algs:
-            u, v = run_alg(picture, start, end, algorithm_name, num_points, patch_size, windowed=bool(i))
+    num_points = 1000
+    patch_size = 51
+    algorithms = ['ssd', 'srd', 'sad', 'bgcsd', 'ncc']
+    noise_param = [None, 1, 2, 4, 8, 16]
+    for picture in pictures:
+        print("Picture: " + picture)
+        for alg in algorithms:
+            print("Algorithm: " + alg)
+            for i in range(2):
+                a = []
+                b = []
+                for j in noise_param:
+                    if j is not None:
+                        u, v = run_alg(picture, start, end, alg, num_points, patch_size, j, windowed=bool(i))
+                    else:
+                        u, v = run_alg(picture, start, end, alg, num_points, patch_size, windowed=bool(i))
+                    a.append(u)
+                    b.append(v)
+                a = pd.DataFrame(np.array(a), columns=['u'])
+                b = pd.DataFrame(np.array(b), columns=['v'])
+                out = pd.concat([a, b], axis=1)
+                if i:
+                    out.to_csv("../data/" + picture + "/windowed_ " + alg + ".csv", ',', index=False)
+                else:
+                    out.to_csv("../data/" + picture + "/" + alg + ".csv", ',', index=False)
